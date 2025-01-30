c05e6d6119da4aaa68f687ee69a4db430f71bbe6
Utkarsh Pathak <utkarshpathakji285@gmail.com>
2025-01-30 13:05:50 +0530

    added a update

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:"https://backend-t8k7.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
