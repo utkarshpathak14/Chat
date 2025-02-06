6131586c0737cfa390a5d8409befc97d5d483ff4
Utkarsh Pathak <utkarshpathakji285@gmail.com>
2025-02-06 11:13:17 +0530

    added a update 7

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -39,6 +39,7 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage />: <Navigate to="/login"/>} />
       </Routes>
+      
 
 
       <Toaster />
